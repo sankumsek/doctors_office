cb3177015e9824a0d4c083140628e8841c85d42f
Sanjeev Sekar <student@epicodus.com>
2014-08-07 09:52:06 -0700

    Initial Commit

---
+++ lib/doctor.rb
@@ -0,0 +1,12 @@
+class Doctor
+attr_reader :name
+
+def initialize attributes
+  @name = name
+end
+
+def name
+  @name
+end
+
+end
---
+++ spec/doctor_spec.rb
@@ -0,0 +1,7 @@
+require 'pg'
+require 'rspec'
+
+describe 'doctor' do
+  it 'initializes doctor name' do
+    expect name.to be_an_instance_of
+end
