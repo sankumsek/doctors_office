70cff9e5fb1a17399e80f9b37e87b60c9203eada
Sanjeev Sekar <sankumsek@gmail.com>
2014-08-20 19:43:22 -0700

    Add to spec description of specialty_spec

--- spec/specialty_spec.rb
+++ spec/specialty_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 require 'specialty.rb'
 
 describe 'Specialty' do
-  it 'initializes w/ hash' do
+  it 'initializes with name' do
     specialty = Specialty.new({:name=>'Orthopedic'})
     expect(specialty).to be_an_instance_of Specialty
   end
 
-  it 'empty array' do
+  it 'starts of as an empty array' do
       expect(Specialty.all).to eq []
   end
 
-  it 'saves' do
+  it 'saves multiple names of specialties' do
     specialty1 = Specialty.new({:name=>'Orthopedic'})
     specialty1.save
     specialty2 = Specialty.new({:name=>'Gynecologist'})
@@ -21,7 +21,7 @@ describe 'Specialty' do
     expect(Specialty.all).to eq [specialty1, specialty2, specialty3]
   end
 
-  it 'removes' do
+  it 'removes specific name of specialty' do
     specialty = Specialty.new({:name=>'Orthopedic'})
     specialty.save
     specialty.remove
